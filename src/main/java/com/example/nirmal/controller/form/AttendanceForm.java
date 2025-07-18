33ab1daf7046e0f956c07feda511baae02393096
kobayashi_yuzuri <kobayashi.yuzuri@alhinc.jp>
2025-07-18 18:06:09 +0900

    7/18　18:00 更新

--- src/main/java/com/example/nirmal/controller/form/AttendanceForm.java
+++ src/main/java/com/example/nirmal/controller/form/AttendanceForm.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -19,8 +20,9 @@ public class AttendanceForm {
     private String breakStart;
     @Pattern(regexp = "^([01][0-9]|2[0-3]):[0-5][0-9]$", message = "時刻を入力してください")
     private String breakEnd;
-    private Short status;
     private Short workStatus;
+    private Short status;
+    @DateTimeFormat(pattern = "yyyy-MM-dd 00:00:00.0")
     private Date workDate;
     private int userId;
     private Date createdDate;
--- src/main/java/com/example/nirmal/dto/workCalendar.java
+++ src/main/java/com/example/nirmal/dto/workCalendar.java
@@ -2,6 +2,7 @@ package com.example.nirmal.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.sql.Time;
 import java.util.Date;
@@ -9,6 +10,7 @@ import java.util.Date;
 @Getter
 @Setter
 public class workCalendar {
+    @DateTimeFormat(pattern = "yyyy-MM-dd 00:00:00.0")
     private Date date;
     private String dayofweek;
     private int year;
@@ -17,8 +19,8 @@ public class workCalendar {
     private String workEnd;
     private String breakStart;
     private String breakEnd;
-    private Short status;
     private Short workStatus;
+    private Short status;
     private Integer id;
     private Date createdDate;
     private Date updatedDate;
